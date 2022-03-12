15b808eb8c0eb7233a3c341c09a7bb1e47bcaf48
Gabor Pfalzer <pfalzer@gmail.com>
2022-03-12 13:25:06 +0000

    add footer

--- components/footer/Footer.Styles.tsx
+++ components/footer/Footer.Styles.tsx
@@ -1,10 +1,25 @@
 import styled from "@emotion/styled";
+import { colors } from "../../styles/variables";
 
-const Footer = styled.footer({});
+const Footer = styled.footer({
+  marginTop: "88px",
+  width: "100%",
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "column",
+});
 
-const FooterImageContainer = styled.div({});
+const FooterImageContainer = styled.div({
+  marginBottom: "8px",
+});
 
-const FooterParagraph = styled.p({});
+const FooterParagraph = styled.p({
+  fontFamily: "Source Code Pro, serif",
+  fontSize: "14px",
+  fontWeight: 400,
+  color: colors.darkBlue,
+  marginBottom: "8px",
+});
 
 export const Styled = {
   Footer,
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import { Styled } from "./Footer.Styles";
 
 const Footer: React.FC = () => {
   return (
     <Styled.Footer>
-      <Styled.FooterImageContainer></Styled.FooterImageContainer>
+      <Styled.FooterImageContainer>
+        <Image
+          src="/images/coffee.png"
+          alt="Graphic of a cup of steaming coffee"
+          height={44}
+          width={58}
+          layout="fixed"
+        />
+      </Styled.FooterImageContainer>
       <Styled.FooterParagraph>
         r.a.kertesz@gmail.com / (+34) 644 265 695
       </Styled.FooterParagraph>
